f9f35dfccceeb5b6e805a41904bcb0443b266fcb
sergey <filimonovser2004@gmail.com>
2026-06-10 13:33:06 +0300

    better drop system, mold, mold damage area

--- items/Items_Scripts/ItemPurpose.gd
+++ items/Items_Scripts/ItemPurpose.gd
@@ -2,12 +2,7 @@ extends Node
 class_name ItemActions
 
 static var actions = {
-	1: func(_player): 
-		print("Iron has no use yet"),
-	
-	2: func(_player):
-		print("Steel has no use yet"),
-	
+	# Только предметы, которые можно использовать
 	3: func(player):
 		if player.radiation > 0:
 			player.radiation = max(player.radiation - 15, 0)
@@ -15,9 +10,11 @@ static var actions = {
 			return true
 		return false,
 }
-
+##короче я добавлю типа +0.01 защиты за каждое применение и шейдер чтобы можно было набухаться и стать имбой на короткий срок
+			#но рил много надо типа стак или два
 static func use(player, item_id: int) -> bool:
 	if actions.has(item_id):
 		var result = actions[item_id].call(player)
 		return result if result != null else true
+	# Предмет без действия — использовать нельзя
 	return false
